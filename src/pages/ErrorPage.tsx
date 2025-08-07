fe4d97f3180c3ed840ea28c096ab78464ef00035
diuyuy <yshyun0105@gmail.com>
2025-08-07 20:33:20 +0900

    feat: atomic component 추가

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,11 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router';
+import NotFoundPage from './NotFoundPage';
+
 export default function ErrorPage() {
-  return <>Error Page</>;
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error) && error.status === 404)
+    return <NotFoundPage />;
+
+  return <>ErrorPage</>;
 }
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@/components/ui/button';
+import { ROUTE_PATHS } from '@/constants/routepaths';
+import { Link } from 'react-router';
+
+export default function NotFoundPage() {
+  return (
+    <main className='grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8'>
+      <div className='text-center'>
+        <p className='text-base font-semibold text-primary'>404</p>
+        <h1 className='mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl'>
+          Page not found
+        </h1>
+        <p className='mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8'>
+          Sorry, we couldn’t find the page you’re looking for.
+        </p>
+        <div className='mt-10 flex items-center justify-center gap-x-6'>
+          <Button asChild>
+            <Link to={ROUTE_PATHS.MAIN}>Go back home</Link>
+          </Button>
+          <a href='#' className='text-sm font-semibold text-gray-900'>
+            Contact support <span aria-hidden='true'>&rarr;</span>
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
