5e31b967e2a6c70e02569da529fa63b0a952c970
11hertz <reachzin@naver.com>
2025-08-13 21:25:28 +0900

    feat: TodaysKeywordCard component ui 작업 완료

--- src/features/keywords/components/TodaysKeywordCard.tsx
+++ src/features/keywords/components/TodaysKeywordCard.tsx
@@ -1,3 +1,23 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { FaSearch } from 'react-icons/fa';
+
 export default function TodaysKeywordCard() {
-  return <div>TodaysKeywordCard</div>;
+  return (
+    <Card className='min-w-125 my-5 border-2'>
+      <CardHeader className='text-center'>
+        <CardTitle className='text-2xl font-extrabold flex gap-3 items-center justify-center'>
+          <FaSearch />
+          <span>오늘의 키워드</span>
+        </CardTitle>
+        <CardDescription className='my-3 text-5xl font-extrabold text-primary'>
+          "주식"
+        </CardDescription>
+      </CardHeader>
+    </Card>
+  );
 }
