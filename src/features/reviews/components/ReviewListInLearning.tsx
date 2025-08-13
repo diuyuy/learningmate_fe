eac558ff79e643488451b9ee7baa65b1f611cba5
11hertz <reachzin@naver.com>
2025-08-13 22:24:15 +0900

    feat: ReviewListInLearning component ui 작업 완료

--- src/features/reviews/components/ReviewListInLearning.tsx
+++ src/features/reviews/components/ReviewListInLearning.tsx
@@ -1,3 +1,21 @@
+import { Button } from '@/components/ui/button';
+import ArticleReview from '@/features/articles/components/ArticleReview';
+
 export default function ReviewListInLearning() {
-  return <div>ReviewListInLearning </div>;
+  return (
+    <article className='w-4/5'>
+      <aside className='mt-5 mb-2'>
+        <h1 className='text-2xl font-bold'>최신 리뷰</h1>
+      </aside>
+      <section className='flex flex-col gap-5'>
+        <ArticleReview />
+        <ArticleReview />
+        <ArticleReview />
+        <ArticleReview />
+        <div className='my-3 flex justify-center'>
+          <Button className='w-30 cursor-pointer'>더보기</Button>
+        </div>
+      </section>
+    </article>
+  );
 }
