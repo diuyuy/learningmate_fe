59d8892ebf021ec5a1084b2df69abbbc1d705f5a
11hertz <reachzin@naver.com>
2025-08-13 21:53:42 +0900

    feat: VideoPlayer component ui 작업 완료

--- src/features/videos/components/VideoPlayer.tsx
+++ src/features/videos/components/VideoPlayer.tsx
@@ -1,3 +1,17 @@
 export default function VideoPlayer() {
-  return <div>VideoPlayer</div>;
+  const src = 'https://www.youtube.com/embed/vbjLJnlB2kg';
+
+  return (
+    <article className='w-4/5 h-full'>
+      <figure className='aspect-video'>
+        <iframe
+          src={src}
+          title='Video Player'
+          className='w-full h-full rounded-2xl shadow-md'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        />
+      </figure>
+    </article>
+  );
 }
