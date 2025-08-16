0a0064b817062973f09b63d9dc17bff11b1b9932
diuyuy <yshyun0105@gmail.com>
2025-08-16 20:55:21 +0900

    feat: 기사 목록 컴포넌트 #9

--- src/features/articles/components/ArticleCard.tsx
+++ src/features/articles/components/ArticleCard.tsx
@@ -1,10 +1,12 @@
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { useFormattedDate } from '@/features/reviews/hooks/useFormattedDate';
 import type { ArticlePreview } from '../types/types';
 
 type Props = {
@@ -12,24 +14,37 @@ type Props = {
 };
 
 export default function ArticleCard({ articlePreview }: Props) {
+  const date = useFormattedDate(articlePreview.publishedAt, {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+
   return (
     <article className='w-full'>
-      <Card className='flex flex-row min-h-10'>
+      <Card className='flex flex-row justify-between min-h-10'>
         <figure className='w-1/10'>
-          <Avatar className='w-15 h-15 ml-5 rounded-lg'>
-            <AvatarImage src='https://github.com/shadcn.png' />
-          </Avatar>
+          <div className='flex flex-col justify-center items-start gap-2 ml-5'>
+            <Avatar className='w-15 h-15 rounded-lg'>
+              <AvatarImage src='https://github.com/shadcn.png' />
+            </Avatar>
+            <small className='text-xs w-15'>{articlePreview.press}</small>
+          </div>
         </figure>
         <section className='w-9/10 ml-3'>
           <CardHeader>
-            <CardTitle className='text-base sm:text-2xl'>
+            <CardTitle className='text-base'>
               <h1>{articlePreview.title}</h1>
             </CardTitle>
-            <CardDescription className='flex justify-between'>
-              <h4>{articlePreview.press}</h4>
-              <h4>{articlePreview.publishedAt}</h4>
-            </CardDescription>
           </CardHeader>
+          <CardDescription className='flex justify-between px-6 mb-2'>
+            <small>{date}</small>
+          </CardDescription>
+          <CardContent className='px-6 text-sm'>
+            <p className='line-clamp-2'>{articlePreview.content}</p>
+          </CardContent>
         </section>
       </Card>
     </article>
