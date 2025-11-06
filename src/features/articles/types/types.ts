68d6d8945fa134098ee1b444d74ec881aca8ac0f
11hertz <reachzin@naver.com>
2025-11-06 20:45:06 +0900

    refactor: articleCard, reviewCard UI 리팩토링

--- src/features/articles/types/types.ts
+++ src/features/articles/types/types.ts
@@ -1,23 +1,16 @@
-import type { Keyword } from '@/features/keywords/types/types';
-
 export type Article = {
   id: number;
-  keyword: Keyword;
   title: string;
   content: string;
-  link: string;
-  reporter: string;
   publishedAt: string;
-  press: string;
   summary: string;
-  views: number;
   scrapCount: number;
-  createdAt: string;
-  updatedAt: string;
+  views: number;
+  keywordId: number;
   scrappedByMe: boolean;
 };
 
 export type ArticlePreview = Pick<
   Article,
-  'id' | 'title' | 'content' | 'publishedAt' | 'press'
+  'id' | 'title' | 'content' | 'publishedAt'
 >;
