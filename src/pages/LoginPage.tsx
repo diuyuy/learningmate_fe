df2b1d65fedaf3c9dd2473dc53fe3f8fbe6b0c72
diuyuy321 <yshyun0105@gmail.com>
2025-11-24 20:22:47 +0900

    refactor: 네이버 로그인 버튼 임시 제거

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,5 @@
-import { GoogleLoginButton } from '@/components/GoogleLoginButton';
-import { KakaoLoginButton } from '@/components/kakao-login-button';
-import { NaverLoginButton } from '@/components/naver-login-button';
 import { ROUTE_PATHS } from '@/constants/routepaths';
+import OauthLoginButtonList from '@/features/articles/components/OauthLoginButtonList';
 import LoginForm from '@/features/auth/components/LoginForm';
 import { Link } from 'react-router';
 
@@ -21,11 +19,7 @@ export default function LoginPage() {
       <div className='text-center relative text-gray-700 before:content-[""] before:absolute before:left-0 before:top-[50%] before:bg-gray-200 before:h-[1px] before:w-[45%] after:content-[""] after:absolute after:right-0 after:top-[50%] after:bg-gray-200 after:h-[1px] after:w-[45%] my-8'>
         or
       </div>
-      <div className='space-y-4'>
-        <GoogleLoginButton />
-        <NaverLoginButton />
-        <KakaoLoginButton />
-      </div>
+      <OauthLoginButtonList />
     </div>
   );
 }
